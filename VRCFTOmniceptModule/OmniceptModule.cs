76a907af8ff36f47a0cc76ab2da22378c6f03a61
cubic <cubicgraphics98@gmail.com>
2024-02-08 13:48:27 +0000

    Cleaned up code, Using vrcft update loop to gather face tracking data from omnicept runtime now.

--- VRCFTOmniceptModule/OmniceptModule.cs
+++ VRCFTOmniceptModule/OmniceptModule.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using HP.Omnicept;
+using HP.Omnicept.Errors;
+using HP.Omnicept.Messaging;
+using HP.Omnicept.Messaging.Messages;
+using Microsoft.Extensions.Logging;
+using NetMQ;
 using System.Reflection;
 using VRCFaceTracking;
 using VRCFTOmniceptModule.EyeLidTools;
@@ -8,23 +13,36 @@ namespace VRCFTOmniceptModule;
 public class OmniceptModule : ExtTrackingModule
 {
     internal static ILogger? logger;
-    private GliaManager? manager;
+    private Glia? m_gliaClient;
     private readonly VRCFTEyeTracking.VRCFTEyeTrackingData Data = new();
+    private bool m_isConnected = false;
 
     public override (bool eyeSuccess, bool expressionSuccess) Initialize(bool eye, bool lip)
     {
-        logger = Logger;
-        if (manager == null)
-            manager = new GliaManager();
-        bool start = manager.StartGlia();
-        if (start)
-            SmoothFloatWorkers.Init();
 
-        manager.OnEyeTracking += tracking => //When new ft recieved from omnicept, update
+        try
         {
-            Data.Update(tracking);
-            VRCFTEyeTracking.UpdateEyeTrackingData(Data);
-        };
+            m_gliaClient = new Glia("VRCFTOmniceptModule",
+                new SessionLicense(String.Empty, String.Empty, LicensingModel.Core, false));
+            SubscriptionList sl = new()
+            {
+                Subscriptions =
+                {
+                    new Subscription(MessageTypes.ABI_MESSAGE_EYE_TRACKING, String.Empty, String.Empty,
+                        String.Empty, String.Empty, new MessageVersionSemantic("1.0.0"))
+                }
+            };
+            m_gliaClient.setSubscriptions(sl);
+            m_isConnected = true;
+        }
+        catch (Exception e)
+        {
+            Logger?.LogError("[VRCFTOmniceptModule] Failed to load for reason {E}", e);
+        }
+        if (m_isConnected)
+        {
+            SmoothFloatWorkers.Init();
+        }
 
         List<Stream> streams = new()
             {Assembly.GetExecutingAssembly().GetManifestResourceStream("VRCFTOmniceptModule.HMD.png")!};
@@ -34,20 +52,47 @@ public class OmniceptModule : ExtTrackingModule
             StaticImages = streams
         };
 
-        Logger.LogDebug("[VRCFTOmniceptModule] Init status {Start}", start);
-        return (start, false);
+        Logger?.LogDebug("[VRCFTOmniceptModule] Init status {Start}", m_isConnected);
+        return (m_isConnected, false);
     }
 
     public override void Update()
     {
-        //We just stop this from using an entire cpu core by making it sleep
-        Thread.Sleep(200);
+        if (m_isConnected)
+        {
+            ITransportMessage? transportMessage = null;
+            try
+            {
+                transportMessage = m_gliaClient!.Connection.Receive(100);
+            }
+            catch (TransportError e)
+            {
+                Logger?.LogDebug("[VRCFTOmniceptModule] TransportError {e}", e);
+            }
+            catch (TerminatingException e)
+            {
+                Logger?.LogDebug("[VRCFTOmniceptModule] TerminatingException {e}", e);
+            }
+            if (transportMessage != null)
+            {
+                if (transportMessage.Header.MessageType == MessageTypes.ABI_MESSAGE_EYE_TRACKING)
+                {
+                    Data.Update(m_gliaClient!.Connection.Build<EyeTracking>(transportMessage));
+                    VRCFTEyeTracking.UpdateEyeTrackingData(Data);
+                }
+            }
+        }
     }
 
     public override void Teardown()
     {
-        manager?.StopGlia();
-        manager = null;
+        if (m_gliaClient != null)
+        {
+            m_gliaClient.Dispose();
+            m_gliaClient = null;
+        }
+        m_isConnected = false;
+        Glia.cleanupNetMQConfig();
         SmoothFloatWorkers.Destroy();
     }
 
